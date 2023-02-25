1f99496143e0edbe3fc5a87bec47400d5899a688
sofiesteier <steiersofie@gmail.com>
2023-02-25 20:27:02 +0100

    updaterade login funktionen

--- problem /js/login_register.js
+++ problem /js/login_register.js
@@ -1,14 +1,22 @@
 "use strict"
 
 async function login () {
+    const contacting_server = document.querySelector(".contacting_server");
+    contacting_server.style.display = "block";
+    document.querySelector(".transparent_background").style.display = "flex";
     
     const username = document.querySelector(".username input").value;
     const password = document.querySelector(".password input").value;       
     
     const response = await send_request(`https://teaching.maumt.se/apis/access/?action=check_credentials&user_name=${username}&password=${password}`);
-        
+    
+    contacting_server.style.display = "none";
+
     if(response.ok) {
+        document.querySelector(".transparent_background").style.display = "none";
+        const resource = await response.json();
         console.log("rätt inlogg");
+        console.log(resource);
     } else if (response.status === 404) {
         document.querySelector(".feedback_login").textContent = "Wrong user name or password."
         document.querySelector(".feedback_login").style.backgroundColor = "white";
@@ -18,7 +26,7 @@ async function login () {
 }
 
 function register_layout () {
-
+    document.querySelector(".feedback_login").style.backgroundColor = "";
     document.querySelector(".login_button").style.display = "none";
     document.querySelector(".register_here_link").style.display = "none";
     document.querySelector(".register_button").style.display = "block";
@@ -54,7 +62,7 @@ async function register () {
     try {
         const contacting_server = document.querySelector(".contacting_server");
         contacting_server.style.display = "block";
-        document.querySelector("#wrapper").classList.toggle("blurred_background")
+        document.querySelector(".transparent_background").style.display = "flex";
         
 
         const username = document.querySelector(".username input").value;
@@ -79,6 +87,7 @@ async function register () {
         
 
         if(response.ok) {
+            const resource = await response.json();
             create_statusCode("Registration complete. Please proceed to login.")
         } else if (response.status === 409) {
             create_statusCode("Sorry, that name is taken. Please try another one.");
@@ -94,7 +103,7 @@ async function register () {
 function close_button (event) {
    const element = event.target.parentElement;
    element.style.display = "none";
-   document.querySelector("#wrapper").classList.toggle("blurred_background");
+   document.querySelector(".transparent_background").style.display = "none";
 }
 
 function create_statusCode (string) {
